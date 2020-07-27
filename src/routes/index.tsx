b93f675a5d674205896af64a029b8b22ee38a39a
mateus <mateushbsb@gmail.com>
2020-07-27 20:49:17 -0300

    commited

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Dasboard from  '../pages/Dashboard';
+import Repository from '../pages/Repository';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={Dasboard}/>
+    <Route path="/repository/:repository+" component={Repository}/>
+  </Switch>
+)
+
+export default Routes;
